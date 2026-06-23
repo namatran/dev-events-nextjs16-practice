ce297955dd8af0000749cb657555cece71adf67e
Nam Tran <xnamtran2009@gmail.com>
2026-06-22 22:51:10 -0500

    implement api routes, server actions and caching, and the event details page

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,5 +1,7 @@
+import {NextRequest, NextResponse} from "next/server";
+import { v2 as cloudinary } from 'cloudinary';
+
 import connectDB from "@/lib/mongodb";
-import { NextRequest, NextResponse } from "next/server";
 import Event from '@/database/event.model';
 
 export async function POST(req: NextRequest) {
@@ -16,11 +18,55 @@ export async function POST(req: NextRequest) {
             return NextResponse.json({ message: 'Invalid JSON data format'}, { status: 400 })
         }
 
-        const createdEvent = await Event.create(event);
+        const file = formData.get('image') as File;
 
-        return NextResponse.json({ message: 'Event created successfully', event: createdEvent }, { status: 201 })
-    } catch(e) {
-        console.error(e)
-        return NextResponse.json({ message: 'Event Creation Failed', error: e instanceof Error ? e.message : 'Unknown'}, { status: 400 })
+        if (!file) return NextResponse.json({ message: 'Image file is required'}, { status: 400 })
+        
+        let tags = JSON.parse(formData.get('tags') as string);
+        let agenda = JSON.parse(formData.get('agenda') as string);        
+
+        const arrayBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        const uploadResult = await new Promise((resolve, reject) => {
+            cloudinary.uploader.upload_stream(
+                { 
+                    resource_type: 'image', 
+                    folder: 'DevEvent',
+                    upload_preset: 'nextjs event app'
+                },
+                (error, results) => {
+                    if (error) return reject(error);
+                    resolve(results);
+                }
+            ).end(buffer);
+        });
+
+        event.image = (uploadResult as { secure_url: string }).secure_url;
+
+        const createdEvent = await Event.create({
+            ...event,
+            tags: tags,
+            agenda: agenda,
+        });
+
+        return NextResponse.json({ message: 'Event created successfully', event: createdEvent }, { status: 201 });
+    } catch (e) {
+        console.error(e);
+        return NextResponse.json({ message: 'Event Creation Failed', error: e instanceof Error ? e.message : 'Unknown'}, { status: 500 })
     }
 }
+
+export async function GET() {
+    try {
+        await connectDB();
+
+        const events = await Event.find().sort({ createdAt: -1 });
+
+        return NextResponse.json({ message: 'Events fetched successfully', events}, { status: 200 });
+    } catch(e) {
+        return NextResponse.json({ message: 'Event fetching failed', error: e }, { status: 500 });
+    }
+}
+
+// a route that accepts a slug as input -> returns the event details (and an error) - let ai do the work while we focus on more important aspects
