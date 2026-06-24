6a5b90ba6ce7f08b009902810297b7baa81d7864
Nam Tran <xnamtran2009@gmail.com>
2026-06-23 20:52:43 -0500

    remove .lean() from similar events Se rver Action to load similar events

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -6,7 +6,7 @@ import connectDB from "@/lib/mongodb";
 export const getSimilarEventsBySlug = async (slug: string) => {
     try {
         await connectDB();
-        const event = (await Event.findOne({ slug })).lean();
+        const event = await Event.findOne({ slug });
 
         return await Event.find({ _id: { $ne: event._id }, tags: { $in: event.tags } }).lean();
     } catch {
